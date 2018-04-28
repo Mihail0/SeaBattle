5433751170a589b8576f543404e519ea50615f0b
Mihail0 <bms-7@mail.ru>
2018-04-28 21:06:57 +0700

    Added LineInitializationTest

--- SeaBattle/linetest.h
+++ SeaBattle/linetest.h
@@ -18,4 +18,11 @@ TEST_F(LineTest, LineCreationTest) {
 	ASSERT_TRUE(line != NULL);
 }
 
+TEST_F(LineTest, LineInitializationTest) {
+	ui8 expected = water;
+	for (ui8 i = 0; i < MAPSIZE; i++) {
+		ASSERT_EQ(expected, line[i]);
+	}
+}
+
 #endif

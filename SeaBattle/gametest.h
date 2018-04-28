e059fe22e89ff362b02d6960447879d41afdd8f8
Mihail0 <bms-7@mail.ru>
2018-04-28 15:41:38 +0700

    Added constuctor & destructor to the GameTest class

--- SeaBattle/gametest.h
+++ SeaBattle/gametest.h
@@ -1,9 +1,16 @@
 #ifndef __GAMETEST
 #define __GAMETEST
 
+#include "defs.h"
 
-class GameTest {
-
+class GameTest :
+	public testing::Test {
+private:
+protected:
+	Game* game;
+public:
+	GameTest();
+	virtual ~GameTest();
 };
 
 #endif

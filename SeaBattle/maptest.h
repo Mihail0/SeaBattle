e1d68b551e1268299112b1c52c8e45dae6400ea5
Mihail0 <bms-7@mail.ru>
2018-04-28 17:07:31 +0700

    Added constructor & destructor to the MapTest class

--- SeaBattle/maptest.h
+++ SeaBattle/maptest.h
@@ -2,10 +2,16 @@
 #define __MAPTEST
 
 #include "defs.h"
+#include "map.h"
 
 class MapTest :
 	public testing::Test {
-
+private:
+protected:
+	Map* map;
+public:
+	MapTest();
+	virtual ~MapTest();
 };
 
 #endif

e6367a54d7aeafd8c3864d4f94f58a1b6839a1f4
Mihail0 <bms-7@mail.ru>
2018-04-29 15:20:52 +0700

    Added fire(x,y) simple implementation

--- SeaBattle/map.cpp
+++ SeaBattle/map.cpp
@@ -1,5 +1,15 @@
 #include "map.h"
 
+void Map::fire(const ui8 &x, const ui8 &y) {
+	//todo method is not complete
+	if (y < 2) {
+		container[0][y] = bomb;
+	}
+	else {
+		throw std::out_of_range("Array index is out of range");
+	}
+}
+
 Line& Map::operator[](const ui8 &index) {
 	return container[index];
 }

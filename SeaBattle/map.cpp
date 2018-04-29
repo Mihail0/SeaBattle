d28ebe921263fecb824716b521a49db9d5e32439
Mihail0 <bms-7@mail.ru>
2018-04-29 15:36:54 +0700

    Completed fire(x,y) implementation

--- SeaBattle/map.cpp
+++ SeaBattle/map.cpp
@@ -1,9 +1,14 @@
 #include "map.h"
 
+/**
+* Fired at the target cell of map
+* @param x - x-coordinate of the target
+* @param y - y-coordinate of the target
+* @note x & y should be less than the mapsize
+*/
 void Map::fire(const ui8 &x, const ui8 &y) {
-	//todo method is not complete
-	if (x < 10) {
-		container[x][0] = bomb;
+	if (x < 10 && y < 10) {
+		container[x][y] = bomb;
 	}
 	else {
 		throw std::out_of_range("Array index is out of range");

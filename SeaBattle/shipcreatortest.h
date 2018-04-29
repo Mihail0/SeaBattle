1c2c4e98724579a012aa8cf7c7b58560d8e6a691
Mihail0 <bms-7@mail.ru>
2018-04-29 20:09:56 +0700

    Added ShipCreatorCreateTest

--- SeaBattle/shipcreatortest.h
+++ SeaBattle/shipcreatortest.h
@@ -2,7 +2,9 @@
 #define __SHIPCREATORTEST
 
 #include "defs.h"
+#include "ship.h"
 #include "shipcreator.h"
+#include "map.h"
 
 class ShipCreatorTest :
 	public testing::Test {
@@ -16,6 +18,32 @@ public:
 
 TEST_F(ShipCreatorTest, ShipCreatorCreationTest) {
 	ASSERT_TRUE(shipCreator != NULL);
+}
+
+TEST_F(ShipCreatorTest, ShipCreatorCreateTest) {
+	//Expected variables
+	Map* expectedMap = new Map();
+	Ship** expectedShips = new Ship*[1];
+	expectedShips[0] = new Ship(0, 0);
+	
+	//Initialization
+	(*expectedMap)[0][0] = ship;
+
+	//Actual variables
+	Map* actualMap = new Map();
+	Ship** actualShips = shipCreator->create(actualMap, horizontal, 0, 0, 1);
+
+	//Comparison
+	EXPECT_EQ(expectedMap, actualMap);
+	EXPECT_EQ(expectedShips[0], actualShips[0]);
+
+	//Final cleansing
+	delete actualShips[0];
+	delete[] actualShips;
+	delete actualMap;
+	delete expectedShips[0];
+	delete[] expectedShips;
+	delete expectedMap;
 }
 
 #endif

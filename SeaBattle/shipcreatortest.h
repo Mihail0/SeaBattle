1a62bdb9acca550627f84a2522f63daa611b527e
Mihail0 <bms-7@mail.ru>
2018-04-29 17:43:35 +0700

    Added constructor & destructor to the ShipCreatorTest class

--- SeaBattle/shipcreatortest.h
+++ SeaBattle/shipcreatortest.h
@@ -2,10 +2,16 @@
 #define __SHIPCREATORTEST
 
 #include "defs.h"
+#include "shipcreator.h"
 
 class ShipCreatorTest :
 	public testing::Test {
-
+private:
+protected:
+	ShipCreator* shipCreator;
+public:
+	ShipCreatorTest();
+	virtual ~ShipCreatorTest();
 };
 
 #endif

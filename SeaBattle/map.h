073596e81f2de7d8206d18a6c05bd361ac14694c
Mihail0 <bms-7@mail.ru>
2018-04-30 17:28:02 +0700

    Added two comparison operators to the Map class

--- SeaBattle/map.h
+++ SeaBattle/map.h
@@ -10,6 +10,8 @@ protected:
 	Line* container;
 public:
 	virtual void fire(const ui8 &, const ui8 &);
+	bool operator==(const Map &) const;
+	bool operator!=(const Map &) const;
 	Line& operator[](const ui8 &);
 	Map();
 	virtual ~Map();

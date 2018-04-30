916f76ae159df02226022953cfdb071e0afe1f80
Mihail0 <bms-7@mail.ru>
2018-04-30 21:04:46 +0700

    Refactored ShipCreator's create method #1

--- SeaBattle/shipcreator.cpp
+++ SeaBattle/shipcreator.cpp
@@ -2,9 +2,25 @@
 
 Ship** ShipCreator::create(Map* &map, const ui8 &direction, const ui8 &x, const ui8 &y, const ui8 &length) {
 	//todo method is not complete
-	Ship** ships = new Ship*[1];
-	ships[0] = new Ship(0, 0);
-	(*map)[0][0] = ship;
+	if (!length) throw std::out_of_range("Array index is out of range");
+	Ship** ships = new Ship*[length];
+	for (ui8 i = 0; i < length; i++) {
+		ships[i] = NULL;
+	}
+	ui8* counter = new ui8();
+	*counter = length;
+	for (ui8 i = 0; i < length; i++) {
+		if (i >= MAPSIZE) {
+			for (ui8 j = 0; j < length; j++) {
+				delete ships[j];
+			}
+			delete[] ships;
+			if (counter) delete counter;
+			throw std::out_of_range("Array index is out of range");
+		}
+		ships[i] = new Ship(i, 0, counter);
+		(*map)[i][0] = ship;
+	}
 	return ships;
 }
 

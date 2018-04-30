0b4123df72f4c970ff3e5cef698f33ddc7611f45
Mihail0 <bms-7@mail.ru>
2018-04-30 17:04:39 +0700

    Added create method simple implementation

--- SeaBattle/shipcreator.cpp
+++ SeaBattle/shipcreator.cpp
@@ -1,5 +1,12 @@
 #include "shipcreator.h"
 
+Ship** ShipCreator::create(Map* &map, const ui8 &direction, const ui8 &x, const ui8 &y, const ui8 &length) {
+	//todo method is not complete
+	Ship** ships = new Ship*[1];
+	ships[0] = new Ship(0, 0);
+	return ships;
+}
+
 ShipCreator::ShipCreator() {
 
 }

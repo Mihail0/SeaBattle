64f10a672259ef976759d73c209dcaa8f67d727d
Mihail0 <bms-7@mail.ru>
2018-04-30 17:08:39 +0700

    Added two comparison operators to the Line class

--- SeaBattle/line.h
+++ SeaBattle/line.h
@@ -9,6 +9,8 @@ protected:
 	ui8* elements;
 public:
 	virtual void fire(const ui8 &);
+	bool operator==(const Line &) const;
+	bool operator!=(const Line &) const;
 	ui8& operator[](const ui8 &);
 	Line();
 	virtual ~Line();

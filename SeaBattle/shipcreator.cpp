fca72484bdfe003306c26fea0a2e7fb5ac209ac9
Mihail0 <bms-7@mail.ru>
2018-04-30 18:33:07 +0700

    Fixed ShipCreator's create method

--- SeaBattle/shipcreator.cpp
+++ SeaBattle/shipcreator.cpp
@@ -4,6 +4,7 @@ Ship** ShipCreator::create(Map* &map, const ui8 &direction, const ui8 &x, const 
 	//todo method is not complete
 	Ship** ships = new Ship*[1];
 	ships[0] = new Ship(0, 0);
+	(*map)[0][0] = ship;
 	return ships;
 }
 

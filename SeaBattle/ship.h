af895e60f294187ec4affb5f1915452388255ece
Mihail0 <bms-7@mail.ru>
2018-04-30 16:34:00 +0700

    Added new constructor Ship(x,y) to the Ship class

--- SeaBattle/ship.h
+++ SeaBattle/ship.h
@@ -5,12 +5,14 @@
 
 class Ship {
 	friend class ShipTest;
+	friend class ShipCreator;
 private:
 protected:
 	ui8 x, y;
 	ui8* count;
 public:
 	Ship();
+	Ship(const ui8 &, const ui8 &);
 	virtual ~Ship();
 };
 

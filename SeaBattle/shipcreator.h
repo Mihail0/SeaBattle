85ca62f0c8f92ff87abc73d41f6a86d63e6e9efa
Mihail0 <bms-7@mail.ru>
2018-04-30 16:54:38 +0700

    Added create method to the ShipCreator class

--- SeaBattle/shipcreator.h
+++ SeaBattle/shipcreator.h
@@ -2,11 +2,14 @@
 #define __SHIPCREATOR
 
 #include "defs.h"
+#include "ship.h"
+#include "map.h"
 
 class ShipCreator {
 private:
 protected:
 public:
+	virtual Ship** create(Map* &, const ui8 &, const ui8 &, const ui8 &, const ui8 &);
 	ShipCreator();
 	virtual ~ShipCreator();
 };

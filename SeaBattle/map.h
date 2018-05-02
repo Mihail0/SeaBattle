92d48396f68c02a4024b4ee88e5b904df57e68b4
Mihail0 <bms-7@mail.ru>
2018-05-02 17:12:38 +0700

    Added fire(x,y,ships,l) method to the Map class

--- SeaBattle/map.h
+++ SeaBattle/map.h
@@ -3,6 +3,7 @@
 
 #include "defs.h"
 #include "line.h"
+#include "ship.h"
 
 class Map {
 private:
@@ -10,6 +11,7 @@ protected:
 	Line* container;
 public:
 	virtual void fire(const ui8 &, const ui8 &);
+	virtual void fire(const ui8 &, const ui8 &, Ship*** &, ui8* &);
 	bool operator==(const Map &) const;
 	bool operator!=(const Map &) const;
 	Line& operator[](const ui8 &);
